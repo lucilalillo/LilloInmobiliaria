fe2d5783794680de68c1ed46ba6e65e16d3909f2
Lucila Lillo <lucilaslillo@gmail.com>
2021-03-25 17:17:47 -0300

    Agregar archivos de proyecto.

---
+++ LilloInmobiliaria/Models/Propietario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LilloInmobiliaria.Models
+{
+    public class Propietario
+    {
+        public int Id { get; set; }
+
+        public String Nombre { get; set; }
+
+        public String Dni { get; set; }
+
+        public String Tel { get; set; }
+
+
+
+
+    }
+}
